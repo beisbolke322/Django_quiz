d72e817f557dc902b88401cf52fa9998bec43b46
beisbolke322 <mykolasp999@gmail.com>
2022-08-01 13:26:27 +0300

    define views and ad logo

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
+from .models import Generatorius
 
 # Create your views here.
+
+
+class Klausimai(View):
+    model = Generatorius
+    template_name = 'quiz.html'
+
+    def get(self, request):
+        x = Generatorius.objects.all()
+        return render(request, self.template_name, {'x': x})
